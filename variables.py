45307485d03cad2eb800e06fca0a19900ce98747
IsabelHernandez <marisahfz07@gmail.com>
2023-06-08 13:33:08 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Python es NO tipado 
+name = "Isabel"
+age = "18"
+is_single = True
+print(name)
+print(age)
+print(is_single)
+
+"""
+Hola soy Isabel, tengo 18 años y soltera = true 
+"""
+# Utilizando format String 
+print ("Hola soy {}, tengo {} años y soltera = {}" . format(name, age, is_single))
+print ("Hola soy {name}, tengo {age} años y soltera = {is_single}")
+
